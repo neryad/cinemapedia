7b557078aff4ace915bd35e3425c48b261e95e9a
neryad <dayern.gomez@hotmail>
2023-07-25 10:36:55 -0400

    feat: :sparkles: desing swippper
    desing base of swipper

--- lib/presentation/widgets/movies/movies_swiper.dart
+++ lib/presentation/widgets/movies/movies_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/config/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -27,10 +28,36 @@ class MoviesSlideShow extends StatelessWidget {
 
 class _Slider extends StatelessWidget {
   final Movie movie;
+
   const _Slider({super.key, required this.movie});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 10, offset: Offset(0, 10)),
+        ]);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.backdropPath,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12),
+                  );
+                }
+                return FadeIn(child: child);
+              },
+            )),
+      ),
+    );
   }
 }
